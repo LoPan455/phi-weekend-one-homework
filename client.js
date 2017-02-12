c49bcb6ae90c0938962b2f49647043c02c16362f
Thomas Johander <lopan@ThomasJndersMBP.example.com>
2017-02-12 17:54:56 -0600

    Client.js is fully commented

--- client.js
+++ client.js
@@ -1,25 +1,24 @@
 // Waiting for html to load before running
 $(document).ready(function(){
   // Creating an event listener, something to check if addEmpBtn has had anything happen to it. 'click' says pay attention to clicks
-  $('#addEmpBtn').on('click', function(){
-    console.log('add emp button clicked');
+  $('#addEmpBtn').on('click', function(){ // the following codeblock will execute on a click of the button with ID addEmpBtn
+    console.log('add emp button clicked'); //just to make sure we're getting the button click
     var firstName = $('#firstName').val(); // gets value in firstName input box
     var lastName = $('#lastName').val(); // gets value in lastName input box
     var empId = $('#empId').val(); // gets the value in empID field
     var empTitle = $('#empTitle').val(); // gets the value in empTitle field
     var empSalary = Number($('#empSalary').val()); // gets the value in empSalary field
-    $('#empTable').append('<tr>'+
-      '<td data-firstname="'+firstName+'">'+firstName+
-      '</td><td data-lastname="'+lastName+'">'+lastName+
-      '</td><td data-empid="'+empId+'">'+empId+
-      '</td><td data-empTitle="'+empTitle+'">'+empTitle+
-      '</td><td data-empsalary="'+empSalary+'" id="salary">'+empSalary+'</td>'+
+    $('#empTable').append('<tr>'+ // This next jQuery statement will add a table row with table data cells for each piece of emplouee data
+      '<td data-firstname="'+firstName+'">'+firstName+'</td>'+
+      '<td data-lastname="'+lastName+'">'+lastName+'</td>'+
+      '<td data-empid="'+empId+'">' + empId +'</td>' +
+      '<td data-empTitle="'+empTitle+'">' + empTitle + '</td>' +
+      '<td data-empsalary="'+empSalary+'" id="salary">'+empSalary+'</td>'+
       '<td><button type="button" name="deleteButton" id="deleteButton">Delete Me!</button></td>'+
       '</tr>'
     );
     totalSalarySpend += empSalary; //increments the total salary spend by each added employee's salary
-    writePerMonthSalarySpend();
-    //$('#monthlySpend > p').html('$' + rndMonthlySalarySpend); //writes the value of rndMonthlySalarySpend to the DOM
+    writePerMonthSalarySpend(); //calls the function writePerMonthSalary spend to calculate the new monthly spend with the new data for each added employee
     $('#firstName').val(''); //clears out input fields
     $('#lastName').val(''); //clears out input fields
     $('#empId').val(''); //clears out input fields
@@ -30,9 +29,9 @@ $(document).ready(function(){
   //Adding in the listener and functionality for the Delete Me! button
   $('#empTable').on('click', '#deleteButton', function(){
     console.log('deleteButton was clicked');
-    totalSalarySpend -= ($(this).parent().prev().data('empsalary'));
-    writePerMonthSalarySpend();
-    $(this).parent().parent().remove();
+    totalSalarySpend -= ($(this).parent().prev().data('empsalary')); //removes the salary amount of the removed employee by traversing up the DOM and getting the prev() 'td element which contains the salary data'
+    writePerMonthSalarySpend(); //recalculates the monthly spend
+    $(this).parent().parent().remove(); //removes the entire 'tr' element that contained the button
   });
 
 });
@@ -41,8 +40,8 @@ var totalSalarySpend = 0; //variable to get the total of all employees' annual s
 var monthlySalarySpend = 0; //variable to get the total spend per month on all the employees Salary
 
   function writePerMonthSalarySpend(){ //runs whenever we need to calculate or re-calculate the monthly salary spend
-    monthlySalarySpend = totalSalarySpend / 12;
-    var rndMonthlySalarySpend = monthlySalarySpend.toFixed(2);
-    $('#monthlySpend > p').html('$' + rndMonthlySalarySpend);
+    monthlySalarySpend = totalSalarySpend / 12; //divides the total salary spend by 12 for a monthly figure
+    var rndMonthlySalarySpend = Number(monthlySalarySpend.toFixed(2)); //rounds the value to up to 2 decimal places and converts this value to a number
+    $('#monthlySpend > p').html('$' + rndMonthlySalarySpend); //writes out the new value in the #monthly spend div
     return
  }
